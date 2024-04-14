2e1df8274db08292b15147bd918342080232ef9b
James Roi Dela Cruz <dcjamesroi@gmail.com>
2024-04-14 17:58:59 +0800

    Added global exception handler

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.response import JSONResponse
 from app.v1.routes import router as apiv1_router 
 
 app = FastAPI()
+
+@app.exception_handler(Exception)
+async def general_exception_handler(request: Request, exc: Exception):
+    # Determine the appropriate status code
+    status_code = exc.status_code if isinstance(exc, HTTPException) else 500
+
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "status": status_code,
+            "data": None,
+            "message": str(exc)  # Provide the original error message
+        }
+    )
 
 app.include_router(apiv1_router, prefix="/api/v1")  # Mount API routes
 
